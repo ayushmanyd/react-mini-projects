1c030bbd9a08848f2f2e8478f383b3106b2cdbe4
Ayushman Pratap Yadav <155382910+ayushmanyd@users.noreply.github.com>
2024-01-10 23:53:47 +0530

    Added Recoil
    Added Recoil to decrease rendering.

--- counter-game/src/App.jsx
+++ counter-game/src/App.jsx
@@ -2,45 +2,49 @@ import { useState } from "react";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Card from "@mui/material/Card";
+import { RecoilRoot, atom, useSetRecoilState, useRecoilValue } from "recoil";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div style={{ display: "flex", justifyContent: "center", margin: 100 }}>
-        <Card style={{ padding: 30, width: 500, height: 200 }}>
-          <Typography variant="h2" textAlign={"center"}>
-            Counter Game
-          </Typography>
-          <br />
-          <br />
-          <br />
-          <Buttons count={count} setCount={setCount} />
-          <br />
-          <CountComponent count={count} />
-        </Card>
-      </div>
+      <RecoilRoot>
+        <div style={{ display: "flex", justifyContent: "center", margin: 100 }}>
+          <Card style={{ padding: 30, width: 500, height: 200 }}>
+            <Typography variant="h2" textAlign={"center"}>
+              Counter Game
+            </Typography>
+            <br />
+            <br />
+            <br />
+            <Buttons />
+            <br />
+            <CountComponent />
+          </Card>
+        </div>
+      </RecoilRoot>
     </>
   );
 }
 
-function Buttons({ count, setCount }) {
+function Buttons() {
   return (
     <div style={{ display: "flex", justifyContent: "space-between" }}>
-      <Increase count={count} setCount={setCount} />
-      <Decrease count={count} setCount={setCount} />
+      <Increase />
+      <Decrease />
     </div>
   );
 }
 
-function Increase({ count, setCount }) {
+function Increase() {
+  const setCount = useSetRecoilState(countState);
   return (
     <div>
       <Button
         variant="contained"
         onClick={() => {
-          setCount(count + 1);
+          setCount((eCount) => eCount + 1);
         }}
       >
         Increase Counter
@@ -49,13 +53,14 @@ function Increase({ count, setCount }) {
   );
 }
 
-function Decrease({ count, setCount }) {
+function Decrease() {
+  const setCount = useSetRecoilState(countState);
   return (
     <div>
       <Button
         variant="contained"
         onClick={() => {
-          setCount(count - 1);
+          setCount((eCount) => eCount - 1);
         }}
       >
         Decrease Counter
@@ -64,7 +69,8 @@ function Decrease({ count, setCount }) {
   );
 }
 
-function CountComponent({ count }) {
+function CountComponent() {
+  const count = useRecoilValue(countState);
   return (
     <div>
       <Typography variant="h4" textAlign={"center"}>
@@ -75,3 +81,8 @@ function CountComponent({ count }) {
 }
 
 export default App;
+
+const countState = atom({
+  key: "countState",
+  default: 0,
+});
