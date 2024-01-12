34dd7d648171771f9f8b47da745a06be74aaef33
Ayushman Pratap Yadav <155382910+ayushmanyd@users.noreply.github.com>
2024-01-12 23:16:46 +0530

    Added Backend
    Added Backend for Signup & Login Pages.

--- signup-login-page/src/backend.js
+++ signup-login-page/src/backend.js
@@ -2,6 +2,7 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
 const app = express();
+const port = 3000;
 
 app.use(express.json());
 
@@ -70,4 +71,6 @@ app.post('/users/login', (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+})
