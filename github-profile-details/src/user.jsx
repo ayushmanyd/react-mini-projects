5b3fd0a6de0a7be880df9e85486fb0ab8ed09eea
Ayushman Pratap Yadav <155382910+ayushmanyd@users.noreply.github.com>
2024-01-26 23:56:07 +0530

    User file updated
    User file updated.

--- github-profile-details/src/user.jsx
+++ github-profile-details/src/user.jsx
@@ -1,13 +1,49 @@
 export default function UserData({ user }) {
-  const { avatar_url, followers, public_repos, name, login } = user;
+  const {
+    avatar_url,
+    followers,
+    bio,
+    location,
+    public_repos,
+    name,
+    login,
+    repos_url,
+  } = user;
 
   return (
     <div className="user">
-      <div className="profilepic">
-        <img src={avatar_url} alt="User Profile Pic" />
-      </div>
-      <div className="githubid">
-        <a href={`https://github.com/${login}`}>{name || login}</a>
+      <div className="usertext">
+        <div className="profilepic">
+          <img className="pic" src={avatar_url} alt="User Profile Pic" />
+        </div>
+        <div className="githubid">
+          <h3>{name}</h3>
+          <a href={`https://github.com/${login}`} target="_blank">
+            {login}
+          </a>
+        </div>
+
+        <div className="bio">
+          <p>{bio}</p>
+        </div>
+        <div className="location">
+          <p>Location: {location}</p>
+        </div>
+        <div className="publicrepos">
+          <p>
+            Public Repositories:
+            <a
+              href={`https://github.com/${login}?tab=repositories`}
+              target="_blank"
+            >
+              {" "}
+              {public_repos} Repos
+            </a>
+          </p>
+        </div>
+        <div className="follower">
+          <p>Followers: {followers}</p>
+        </div>
       </div>
     </div>
   );
